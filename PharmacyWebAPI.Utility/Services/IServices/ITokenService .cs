b6d7b0b303b752de8cbc855d5632378fe271e23f
Moamen Mostafa <mmoamen270@gmail.com>
2023-04-27 07:17:39 +0200

    Add TokenToData Service

--- PharmacyWebAPI.Utility/Services/IServices/ITokenService .cs
+++ PharmacyWebAPI.Utility/Services/IServices/ITokenService .cs
@@ -3,6 +3,7 @@ using PharmacyWebAPI.Models.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,5 +16,7 @@ namespace PharmacyWebAPI.Utility.Services.IServices
         Task<AuthModelDto> GetTokenAsync(LoginDto model);
 
         Task<string> AddRoleAsync(UserRolesDto model);
+
+        string DataFromToken(string token, Func<Claim, bool> selector);
     }
 }
